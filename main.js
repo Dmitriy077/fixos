d2ccbfcccf52d26f64f77309d2b219f82b88c341
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-11-21 15:33:22 +0500

    21.11

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(function() {
+// Главный слайдер
+	$('.main_slider_slick').slick({
+		// autoplay: true,
+  		// autoplaySpeed: 3000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: true,
+	});
+
+});
