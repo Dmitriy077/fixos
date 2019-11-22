6b598064d24a1729efe3b2741f41e7a20a72f723
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-11-22 13:25:30 +0500

    22.11

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ $(function() {
 		slidesToScroll: 1,
 		arrows: true,
 		dots: true,
+		appendArrows:'.main_slider_arrow',
+		prevArrow:'<div class="main_slider_arrow_prev"><img src="./img/main/left.svg" alt=""></div>',
+		nextArrow:'<div class="main_slider_arrow_next"><img src="./img/main/right.svg" alt=""></div>',
 	});
 
 });
