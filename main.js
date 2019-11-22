6d6c2e0caef10a6e2570b1c22fc4f61ee66a38fe
Dmitriy Chapaev <dsergeevic077@gmail.com>
2019-11-22 18:45:19 +0500

    22.11/2

--- main.js
+++ main.js
@@ -13,4 +13,27 @@ $(function() {
 		nextArrow:'<div class="main_slider_arrow_next"><img src="./img/main/right.svg" alt=""></div>',
 	});
 
+
+
+
+// Ползунок
+
+	$( ".calculator_slider" ).slider({
+ 		range: 'min',
+		value : 200,//Значение, которое будет выставлено слайдеру при загрузке
+		min : 0,//Минимально возможное значение на ползунке
+		max : 1000,//Максимально возможное значение на ползунке
+		step : 1,//Шаг, с которым будет двигаться ползунок
+		create: function( event, ui ) {
+			val = $( ".calculator_slider" ).slider("value");//При создании слайдера, получаем его значение в перемен. val
+			$( ".calculator_slider_content" ).html( val );//Заполняем этим значением элемент с id contentSlider
+			// $( ".calculator_slider" ).css('left', val/10 + '%');
+		},
+    	slide: function( event, ui ) {
+			$( ".calculator_slider_content" ).html( ui.value );//При изменении значения ползунка заполняем элемент с id contentSlider
+			// $( ".calculator_slider" ).css('left', ui.value/10 + '%');
+
+        }
+    });
+
 });
